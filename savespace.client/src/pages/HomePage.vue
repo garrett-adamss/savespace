6aa8459a9a898ed9163c000c808123bd8a9869cb
Garrett Adams <garrettadams27@gmail.com>
2023-02-04 01:46:03 -0700

    Main page DONE

--- savespace.client/src/pages/HomePage.vue
+++ savespace.client/src/pages/HomePage.vue
@@ -1,7 +1,24 @@
 <template>
   
-  <div class="home">
-    <img src="../assets/img/lightstroke.png">
+  <div class="home container-fluid text-light">
+    <div class="row logo-row">
+      <h1 class="col-md-9 col-12 logo">save.space</h1>
+      <button class="btn btn-outline-light col-2 demo-btn">30 second demo</button>
+    </div>
+    <div class="row textarea-row">
+      <textarea class="col-md-4 col-10 offset-1" placeholder="Your event name..."></textarea>
+    </div>
+    <div class="row create-space-row">
+      <button class="btn btn-primary col-md-3 col-10 offset-1 space-btn">Create Space</button>
+    </div>
+    <div class="row">
+      <p class="offset-1 mt-3 find-space col-md-2 col-6 selectable">Find a Space</p>
+    </div>
+    <div class="row slogan-text-row">
+      <h2 class="col-3 offset-1 slogan">
+        Collect all <br/> your photos. <br/> In one place
+      </h2>
+    </div>
   </div>
 </template>
 
@@ -14,12 +31,96 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@media only screen and (max-width: 768px){
+  .home{
+    background-image: url("../assets/img/mobile.png");
+    background-position: center;
+    background-size: cover;
+  }
+  .logo-row{
+    margin: none;
+  }
+  .logo{
+    text-align: center;
+  }
+  .logo-row{
+    margin-top: 2em;  
+  } 
+  .demo-btn{
+    display: none;
+  }
+  textarea{
+  margin-top: 15vh;
+  margin-bottom: 2vh;
+  background: transparent;
+  color: white;
+  font-size: 30px;
+  border: none;
+  outline: none;
+  resize: none;
+  max-height: 8vh;
+}
+  .slogan{
+    display: none;
+  }
+}
+// Desktop
+@media only screen and (min-width: 768px){
+  .home{
+    background-image: url("../assets/img/1440p.png");
+    background-position: center;
+    background-size: cover;
+  }
+  .logo-row{
+    margin-left: 3em;
+    margin-top: 3em;
+  }
+  textarea{
+  margin-top: 20vh;
+  background: transparent;
+  color: white;
+  font-size: 30px;
+  border: none;
+  outline: none;
+  resize: none;
+  max-height: 8vh;
+}
+.create-space-row{
+  margin-top: 4vh;
+}
+}
 .home {
+  position: relative;
   height: 100vh;
   background-color: #1C1832;
-  overflow-x: hidden;
+  overflow-y:hidden;
+  overflow-x:hidden;
 }
-.img{
-  height: 90vh;
+.logo{
+  font-size: 40px;
+}
+.demo-btn{
+  border-radius: 36px;
+  font-size: 20px;
+  margin-top: 10px;
+  padding: 11px;
+}
+.space-btn{
+  border-radius: 17px;
+  font-size: 30px;
+  background-color: #7939FE;
+  padding: 2vh;
+}
+.find-space{
+  font-size: 18px;
+}
+.find-space:hover{
+  cursor: pointer;
+}
+.slogan-text-row{
+  margin-top: 5vh;
+}
+.slogan{
+  font-size: 50px;
 }
 </style>
